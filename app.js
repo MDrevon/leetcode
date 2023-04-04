b44861dc2ef62b73588fe6a666b0c8648d1233d6
MDrevon <MsDrevon@gmail.com>
2023-04-04 07:23:12 -0500

    Two Sum solution

--- app.js
+++ app.js
@@ -50,3 +50,20 @@ var containsDuplicate = function (nums) {
     return false;
   }
 };
+
+/** Two Sum
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(target - nums[i])) {
+      return [map.get(target - nums[i]), i];
+    }
+    map.set(nums[i], i);
+  }
+};
+
+console.log(twoSum([2, 7, 11, 15], 9));
